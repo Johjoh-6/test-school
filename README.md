ce7660a70737b31e905b81d5403343a35991fa0a
Johjoh-6 <six.johann@gmail.com>
2021-04-17 14:59:27 -0400

    Last version, try to do a new slider

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # test-school
-Text for enter in the school
+Text for enter in the school,
+
+# HTML
+-Nav phone already on it, for a futur responsive menu.
+-Slider customer to add news function
+
+# CSS
+-Breakdown responsive already got.
+-responsive to do. 
+-Don't forget to set all the grid !!
+
+# JS
+-look for make a smooth slider.
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,12 @@ const link = document.getElementsByClassName("link-nav");
 let selected = document.getElementsByClassName("selected");
 // Apply style for the nav
 for (let i = 0; i < link.length; i++) {
-    link[i].addEventListener("click", function() {
-        selected[0].className = selected[0].className.replace(" selected", "");
+  link[i].addEventListener("click", function () {
+    selected[0].className = selected[0].className.replace(" selected", "");
     this.className += " selected";
     console.log('selected')
-    });
-  }
+  });
+}
 
 
 
@@ -27,14 +27,20 @@ function dotSlides(n) {
   let i;
   let slides = document.getElementsByClassName("customer");
   let dots = document.getElementsByClassName("slider-button");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" select", "");
+    dots[i].className = dots[i].className.replace(" select", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " select";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " select";
 }
+
+// slider v2
